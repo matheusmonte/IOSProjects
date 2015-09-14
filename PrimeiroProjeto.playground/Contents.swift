9a93f80968630d449615f1587f5390d7257caab2
Matheus Monte <Clark@INDTs-MacBook-Pro-3.local>
2015-09-14 10:42:49 -0400

    CReate the project and a playground
    Signed-off-by: Matheus Monte <Clark@INDTs-MacBook-Pro-3.local>

---
+++ PrimeiroProjeto.playground/Contents.swift
@@ -0,0 +1,6 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+INI_IPV6 teste = 8;
---
+++ TaskManager/TaskManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Clark on 9/14/15.
+//  Copyright (c) 2015 Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
